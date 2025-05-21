22137d8178fc7bf0743a0892cc4236f27804ee38
onyiafranklin <onyiafranklin89@gmail.com>
2025-05-21 11:16:32 +0100

    updated jenkins

---
+++ vault-jenkins/variable.tf
@@ -0,0 +1,9 @@
+variable "domain" {
+  
+description = "The domain name for our pet adoption project"
+  type        = string
+  default     = "set30.space"
+}
+variable "region" {
+    default = "eu-west-1"
+}
