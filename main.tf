73318ba111b0e08764773c17dacadc43ee2c94ea
Ola <olasunkanmi.m.owolabi@gmail.com>
2025-05-30 09:41:04 +0100

    updated the module main file

--- main.tf
+++ main.tf
@@ -21,42 +21,42 @@ module "vpc" {
   az2    = "eu-west-1b"
 }
 
-# module "bastion" {
-#   source      = "./module/bastion"
-#   name        = local.name
-#   vpc         = module.vpc.vpc_id
-#   subnets  = [module.vpc.pub_sub1_id, module.vpc.pub_sub2_id]
-#   keypair     = module.vpc.public_key
-#   privatekey = module.vpc.private_key
-#   nr-acc-id = var.nr-acc-id
-#   nr-key = var.nr-key
-# }
+module "bastion" {
+  source      = "./module/bastion"
+  name        = local.name
+  vpc         = module.vpc.vpc_id
+  subnets  = [module.vpc.pub_sub1_id, module.vpc.pub_sub2_id]
+  keypair     = module.vpc.public_key
+  privatekey = module.vpc.private_key
+  nr-acc-id = var.nr-acc-id
+  nr-key = var.nr-key
+}
 
-# module "ansible" {
-#   source    = "./module/ansible"
-#   name      = local.name
-#   keypair   = module.vpc.public_key
-#   subnet_id = module.vpc.pri_sub1_id
-#   vpc       = module.vpc.vpc_id
-#   bastion   = ""
-#   private-key = module.vpc.private_key
-#   deployment = ""
-#   prod-bashscript = "./module/ansible/prod-bashscript.sh" # Path to the prod bash script
-#   stage-bashscript = "./module/ansible/stage-bashscript.sh" # Path to the stage bash script
-#   nexus-ip = ""
-#   nr-key = ""
-#   nr-acc-id = ""
-# }
-# module "database" {
-#   source    = "./module/database"
-#   name      = local.name
-#   pri-sub-1 = module.vpc.pri_sub1_id
-#   pri-sub-2 = module.vpc.pri_sub2_id
-#   bastion-sg   = module.bastion.bastion-sg
-#   vpc-id    = module.vpc.vpc_id
-#   stage-sg  = module.stage-env.stage-sg
-#   prod-sg   = module.prod-env.prod-sg
-# }
+module "ansible" {
+  source    = "./module/ansible"
+  name      = local.name
+  keypair   = module.vpc.public_key
+  subnet_id = module.vpc.pri_sub1_id
+  vpc       = module.vpc.vpc_id
+  bastion   = ""
+  private-key = module.vpc.private_key
+  deployment = ""
+  prod-bashscript = "./module/ansible/prod-bashscript.sh" # Path to the prod bash script
+  stage-bashscript = "./module/ansible/stage-bashscript.sh" # Path to the stage bash script
+  nexus-ip = ""
+  nr-key = ""
+  nr-acc-id = ""
+}
+module "database" {
+  source    = "./module/database"
+  name      = local.name
+  pri-sub-1 = module.vpc.pri_sub1_id
+  pri-sub-2 = module.vpc.pri_sub2_id
+  bastion-sg   = module.bastion.bastion-sg
+  vpc-id    = module.vpc.vpc_id
+  stage-sg  = module.stage-env.stage-sg
+  prod-sg   = module.prod-env.prod-sg
+}
 
 module "sonarqube" {
   source = "./module/sonarqube"
