5054037b2b176af414fed619cc40fcdb57a9ae8e
mr-lington <99319094+mr-lington@users.noreply.github.com>
2025-06-14 15:44:45 +0100

    Update variable.tf

--- vault-jenkins/variable.tf
+++ vault-jenkins/variable.tf
@@ -2,7 +2,7 @@ variable "domain" {
 
   description = "The domain name for our pet adoption project"
   type        = string
-  default     = "set30.space"
+  default     = "3ureka.com"
 }
 variable "region" {
   default = "eu-west-1"
